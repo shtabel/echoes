a755b7de53c59f27055992a5f7b5577c4ee6a14c
andtarspb <46713402+andtarspb@users.noreply.github.com>
2019-07-15 14:38:23 +0300

    главное меню и меню паузы
    - скрипты для меню
    - интерфейс

---
+++ Echoes/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    // PUBLIC INIT
+    public string startScene;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(startScene);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit game");
+        Application.Quit();
+    }
+}
---
+++ Echoes/Assets/Scripts/PauseMenuScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuScript : MonoBehaviour
+{
+    // PUBLIC INIT
+    //public string newGameScene;
+    public GameObject pauseMenu;
+    public bool isPaused;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            OpenPauseMenu();
+        }
+    }
+
+    public void NewGame()
+    {
+        ResumeGame();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void ResumeGame()
+    {
+        isPaused = false;
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void OpenPauseMenu()
+    {
+        if (isPaused)
+        {
+            ResumeGame();
+        }
+        else
+        {
+            isPaused = true;
+            pauseMenu.SetActive(true);
+            Time.timeScale = 0f;
+        }
+    }
+}
